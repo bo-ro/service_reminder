20a335e90025f23de582b6a586ca383a806c28ac
bo-ro <bobberowiec@gmail.com>
2020-03-21 09:07:26 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'cars#index'
 
-  get "logout", to: 'sessions#destroy'
+  get "/logout", to: 'sessions#destroy'
 end
