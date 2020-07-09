01ef86db61603b488b680d420cfc4e3f92fe84d3
bo-ro <bobberowiec@gmail.com>
2020-07-09 11:58:36 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :cars
   resources :users, only: [:new, :create]
   resource :sessions, only: [:create, :destroy, :new]
+  resources :messages, only: [:new ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
